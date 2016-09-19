9f187ee490a173cc2d588df1e6c0ae70b170af3f
FraSanta89 <francesco.santarelli1989@gmail.com>
2016-09-19 08:41:42 +0200

    Add files via upload

--- Monsters.py
+++ Monsters.py
@@ -1,37 +1,39 @@
 import random
 from Enemy import Enemy
-
-class Monsters:
     
-    class Goblin(Enemy):
+class Goblin(Enemy):
         
-        description = 'a tiny and nasty creature'
-        habitat = 'underground'
-        healthDice = 2
-        dmgDice = 2
+    description = 'a tiny and nasty creature'
+    habitat = 'underground'
+    healthDice = 2
+    dmgDice = 2
 
-    class Ghoul(Enemy):
+class Ghoul(Enemy):
 
-        description = 'a repulsive creature, feeds on corpses.'
-        habitat = 'cemeteries'
-        healthDice = 6
-        dmgDice = 4
+    description = 'a repulsive creature, feeds on corpses.'
+    habitat = 'cemeteries'
+    healthDice = 6
+    dmgDice = 4
 
-    class Ghost(Enemy) :
+class Ghost(Enemy) :
 
-        description = 'a vengeful spirit.'
-        habitat = 'ruins'
-        healthDice = 8
-        dmgDice = 4
-        isMagical = True
+    description = 'a vengeful spirit.'
+    habitat = 'ruins'
+    healthDice = 8
+    dmgDice = 4
+    isMagical = True
 
-    class Dragon(Enemy):
+class Dragon(Enemy):
 
-        description = 'the most dangerous creature'
-        habitat = 'caverns'
-        helathDice = 20
-        dmgDice = 12
-        isMagical = True
+    description = 'the most dangerous creature'
+    habitat = 'caverns'
+    helathDice = 20
+    dmgDice = 12
+    isMagical = True
+    breath = 'fire' 
 
-        def breathe_something():
-            dmg = random.randint(1,6)
+    def breathe(self):
+        print('The dragon is breathing', self.breath, '!')
+        dmg = random.randint(1,6)
+        print('The dragon dealt', dmg, 'damages.')
+        return dmg
