b1adef4ba0ab55a6f7ce3082d851ad84b190c1d7
FraSanta89 <francesco.santarelli1989@gmail.com>
2016-09-19 09:47:06 +0200

    Enemy.py
    get_damage() method added.

--- Enemy.py
+++ Enemy.py
@@ -5,19 +5,31 @@ class Enemy:
     isMagical = False
     isPoisonous = False
     dmgDice = 1
-    
+
     def __init__(self, name, level, healthDice):
+        
         self.name = name
         self.level = level
         self.healthDice = healthDice
-        self.health = level * random.randint(1, healthDice)
+        self.health = level *  healthDice
         
     def fight(self):
+        
         dmg = self.level * random.randint(1, self.dmgDice)
         return dmg
+
+    def get_damage(self, dmg):
+
+        health = self.health
         
-'''    def set_stats(self, const, strength, intel=1, dext=1, char=1):
-        self.const = const
-        self.strength = strength
-        self.intel = intel
-        self.dext = dext'''
+        if (self.isMagical==False and self.health != 0):
+            print('Enemy took', dmg, 'damages.')
+            self.health -= dmg
+
+        if (self.health == 0):
+            print('Enemy is dead.')
+
+en = Enemy('generic enemy', 1, 6)
+print(en.health)
+en.get_damage(6)
+print('Enemy remaining health is now', en.health)        
